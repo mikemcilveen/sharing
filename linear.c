34103123cf6f3d2661e38442e74d6989d1724c24
mikemcilveen <mikemcilveen@gmail.com>
2019-03-08 09:11:36 -0500

    Update linear.c

--- linear.c
+++ linear.c
@@ -23,23 +23,29 @@ int main(void)
     // Use the time as a seed for the random number generator 
     srand(seconds); 
     
-    // Array to store each number 
+    // DECLARING //
+    // Array to store random numbers between 0 and 99 
     int nums[LENGTH];
-    // Array to store how often each number appears
+    // Array of counters for how often each number 0 to 99 appears in nums array
     int counter[100]; 
-    // Variable to store the lowest number that has appeared
+    
+    
+    // INITIALIZING //
+    // Variable to store the random number with the lowest frequency in counter array
     int low = 99;
-    // Variable to store how many times the that lowest number has appeared 
+    // Variable to store the lowest frequency that appears in counter array 
     int low_val = 0; 
-    // Variable to store the highest number that has appeared 
+    // Variable to store the random number with the highest frequency in counter array 
     int high = 0; 
-    // Variable to store how many times the that highest number has appeared
+    // Variable to store highest frequency that appears in counter array
     int high_val = 0; 
     // Set the number of times each number has appeared to zero 
     for(int i = 0; i < 100; i++)
     {
         counter[i] = 0; 
     }
+    
+    // GENERATING FREQUENCIES //
     // Generate all the random numbers and record how often each appears
     for(int i = 0; i < LENGTH; i++)
     {
@@ -48,6 +54,8 @@ int main(void)
         // Add to the number of times that specific number has appeared 
         counter[nums[i]]++; 
     }
+    
+    // ANALYZING THE FREQUENCIES //
     // Check each number to see which appeared the most and least
     for(int i = 0; i < 100; i++)
     {
@@ -64,6 +72,8 @@ int main(void)
             low_val = i; 
         }
     }
+    
+    // OUTPUT
     // Print the lowest & highest numbers and how often each appears 
     printf("The most common number %i appears %i times\n", high_val, high);
     printf("The least common number %i appears %i times\n", low_val, low);
