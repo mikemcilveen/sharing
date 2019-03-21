1898a68576f916846c32540962a8413732d908cb
mikemcilveen <mikemcilveen@gmail.com>
2019-03-21 14:52:20 -0400

    Update ii.c

--- ii.c
+++ ii.c
@@ -6,23 +6,25 @@ int main ()
     
     
     // initialize array
-    int a[] = {2,1,4,3,5};
+    int a[] = {2,1,1,3,0};
     
     // initialize index array
-    int p[5] = {0,1,2,3,4};
+    int p[] = {0,1,2,3,4};
     
     int j = 0;
     for (int i = 1; i < 5; ++i)
     {
+        // start comparing element at i with sorted elements before position i
         j = i - 1;
-        while (a[i] < a[p[j]] && j >=0)
+        
+        while (j >= 0 && a[i] < a[p[j]])
         {
             j = j - 1;
         }
-        p[i] = j + 1;
+        p[j+1] = i;
         for (int k = 0; k < i; ++k)
         {
-            if (p[k] >= j + 1)
+            if (p[k] <= j + 1)
             {
                 p[k] = p[k] + 1;
             }
@@ -37,10 +39,15 @@ int main ()
     }
     
     // sorted 
+    int q[5];
+    for (int i = 0; i < 5; ++i)
+    {
+        q[p[i]] = i;
+    }
     printf("\n\nSorted:\n");
     for (int i = 0; i < 5; ++i)
     {
-        printf("%i ", a[p[i]]);
+        printf("%i ", q[i]);
     }
     printf("\n\n");
 }
