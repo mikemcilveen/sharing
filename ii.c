8e6e7c8047b20366dbf184b80dff155c7feb9d8a
mikemcilveen <mikemcilveen@gmail.com>
2019-03-25 12:13:11 -0400

    Update ii.c

--- ii.c
+++ ii.c
@@ -6,11 +6,12 @@ int main ()
     
     
     // initialize array to be sorted
-    int a[] = {21,14,11,13,30};
+    int a[] = {1,14,1,13,0};
     
     // initialize index array
     // ap[i] = index of a[i] in the sorted array
     int ap[] = {0,1,2,3,4};
+    int sp[] = {0,1,2,3,4};
     
     int j = 0;
     for (int i = 1; i < 5; ++i)
@@ -18,25 +19,37 @@ int main ()
         // start comparing element at i with sorted elements before position i
         j = i - 1;
         
-        while (j >= 0 && a[i] < a[j])
+        while (j >= 0 && a[i] < a[sp[j]])
         {
             j = j - 1;
         }
+        
         ap[i] = j+1;
+        sp[j+1] = i;
+        
         for (int k = 0; k < i; ++k)
         {
             if (ap[k] >= j + 1)
             {
                 ap[k] = ap[k] + 1;
+                sp[ap[k]]=k;
+          
             }
         }
-    }
-    // original 
-    printf("\n\nPointers:\n");
-    for (int i = 0; i < 5; ++i)
+    printf("\n\nPointers to s:\n");
+    for (int l = 0; l < 5; ++l)
     {
-        printf("%i ", ap[i]);
+        printf("%i ", ap[l]);
     }
+    
+    printf("\n\nPointers to a:\n");
+    for (int l = 0; l < 5; ++l)
+    {
+        printf("%i ", sp[l]);
+    }
+    
+    }
+
     
     // original 
     printf("\n\nOriginal:\n");
@@ -46,12 +59,12 @@ int main ()
     }
     
     // sorted 
-    // q[i] = the index of the element in a, with position i in the sorted array
-    int sp[5];
-    for (int i = 0; i < 5; ++i)
-    {
-        sp[ap[i]] = i;
-    }
+    // sp[i] = the index of the element in a, with position i in the sorted array
+    // 
+//    for (int i = 0; i < 5; ++i)
+//    {
+//        sp[ap[i]] = i;
+//    }
     printf("\n\nSorted:\n");
     for (int i = 0; i < 5; ++i)
     {
@@ -59,4 +72,3 @@ int main ()
     }
     printf("\n\n");
 }
-
